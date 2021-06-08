d3696f751b189187b370862f04872692bf54233f
Jean-Jacques Brucker <jeanjacquesbrucker@gmail.com>
2021-06-08 18:33:04 +0200

    improve JSON functions + create a unit test

---
+++ test/pgpi_var_to_json.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+source "$(dirname "$0")/../pgpi" --version
+
+declare -a a=([0]="tableau" [1]="anormal" [2]="où tout" [3]="se suit" [12]="très anormal" [13]=$'form\ffeed...' [15]=$'back\bspace...' [21]="avec\\t des tab\\r" [22]=$'avec\t des tab\r' [42]=$'ça\ndevient\\\nsa/crément"\nle b\'del')
+
+diff -us <(sed -n '/^{$/,${p;/}/q}' "$0") <(pgpi_var_to_json a)
+
+exit $?
+{
+	"0": "tableau",
+	"1": "anormal",
+	"2": "où tout",
+	"3": "se suit",
+	"12": "très anormal",
+	"13": "form\ffeed...",
+	"15": "back\bspace...",
+	"21": "avec\\t des tab\\r",
+	"22": "avec\t des tab\r",
+	"42": "ça\ndevient\\\nsa\/crément\"\nle b'del"
+}
