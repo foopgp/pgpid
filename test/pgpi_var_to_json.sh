3802fd84ab737cd76b420325df8c7a9b40c31256
Jean-Jacques Brucker <jeanjacquesbrucker@gmail.com>
2021-07-17 17:19:48 +0200

    update the unit test

--- test/pgpi_var_to_json.sh
+++ test/pgpi_var_to_json.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-source "$(dirname "$0")/../pgpi" --version
+source "$(dirname "$0")/../pgpid-gen" --version
 
 declare -a a=([0]="tableau" [1]="anormal" [2]="où tout" [3]="se suit" [12]="très anormal" [13]=$'form\ffeed...' [15]=$'back\bspace...' [21]="avec\\t des tab\\r" [22]=$'avec\t des tab\r' [42]=$'ça\ndevient\\\nsa/crément"\nle b\'del')
 
